cebb2dded1ef62c3a7c4386f52ae5deefce5069a
Pedro Kunz <56512310+pedrohkunz@users.noreply.github.com>
2022-05-20 20:19:34 -0300

    Delete src/model directory

--- src/model/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('MYSQL_SERVER', 'localhost');
-    define('MYSQL_DATABASE', 'starchords');
-    define('MYSQL_USER', 'root');
-    define('MYSQL_PASS', '');
-    define('MYSQL_CHARSET', 'utf8');
-?>
